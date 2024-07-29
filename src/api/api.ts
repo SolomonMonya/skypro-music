db0d37bc106a7c85e8ca56516849c129f61d96fa
SolomonMonya <admiral.aleks.d@gmail.com>
2024-07-29 18:30:49 +0300

    Step2 API not found

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,4 @@
-const apiUrlGetTracks = "https://skypro-music-api.skyeng.tech/catalog/track/all/";
+const apiUrlGetTracks = "https://webdev-music-003b5b991590.herokuapp.com/catalog/track/all/";
 
 export async function getTracks() {
   const res = await fetch(apiUrlGetTracks);
